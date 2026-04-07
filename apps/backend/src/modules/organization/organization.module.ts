a276ed807a03e9dfc1ff5aac11152f632dc192bd
Himanshu Bhatt <himanshubhatt2015@gmail.com>
2026-04-07 13:53:04 +0530

    added prisma service in the organation module

--- apps/backend/src/modules/organization/organization.module.ts
+++ apps/backend/src/modules/organization/organization.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
+import { PrismaService } from '../../../prisma/prisma.service';
 
 @Module({
   controllers: [OrganizationController],
-  providers: [OrganizationService]
+  providers: [OrganizationService, PrismaService]
 })
 export class OrganizationModule {}
